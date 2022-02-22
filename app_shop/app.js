653333fa3fe0b69e832c6f048c2709cf013ed512
Adam <adam.midtt@gmail.com>
2022-02-22 20:04:25 +0100

    control

--- app_shop/app.js
+++ app_shop/app.js
@@ -4,5 +4,6 @@ const app = express();
 
 //jak cos uzyje serwer wykonaj te operacje
 app.use((req, res, next) => {
-
+res.status(200).json({wiadomosc: 'wszystko ok'})
 })
+module.exports=app
--- app_shop/server.js
+++ app_shop/server.js
@@ -1,8 +1,14 @@
 // import http
 const http = require('http');
+
+//import app
+const app = require('./app')
+
 //ustawienia portu servera
 const port = process.env.port || 3000;
+
 //tworzenie servera
-const server = http.createServer();
-//włączenie sservera
+const server = http.createServer(app);
+
+//włączenie servera
 server.listen(port);
